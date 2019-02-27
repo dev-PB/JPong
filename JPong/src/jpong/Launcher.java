7e106248b0989fb44ca2ff8e5d00ce4db6420147
onlinePB <ryanwilson210400@gmail.com>
2019-02-27 14:40:31 +0000

    Created Java project. Created the Java project.

---
+++ JPong/src/jpong/Launcher.java
@@ -0,0 +1,17 @@
+package jpong;
+
+/**
+ *
+ * @author Ryan Wilson
+ * @version 0.1.0 27/02/2019
+ */
+public class Launcher {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
