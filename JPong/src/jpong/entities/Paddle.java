b62fd7506372727bef5efb91f68c92a19452456f
onlinePB <ryanwilson210400@gmail.com>
2019-02-27 15:30:34 +0000

    Created constructor method for Paddle Created the constructor method for the Paddle class.

--- JPong/src/jpong/entities/Paddle.java
+++ JPong/src/jpong/entities/Paddle.java
@@ -1,9 +1,33 @@
 package jpong.entities;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 /**
  *
  * @author Ryan Wilson
  */
 public class Paddle {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private int points;
+    private Color colour;
+    
+    public Paddle(int x, int y, int width, int height, Color colour){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.colour = colour;
+        points = 0;
+    }
+    
+    public void renderPaddle(Graphics graphics){
+        graphics.setColor(colour);
+        graphics.fillRect(x,y,width,height);
+    }
+    
     
 }
