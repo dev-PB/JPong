88b6405a409a01baf93e4338dd151d1684075f26
onlinePB <ryanwilson210400@gmail.com>
2019-02-27 15:19:41 +0000

    Created run(), start() & stop() methods in Game Created the run(), start() and stop() methods in the Game class. Also created stubs for the init(), update(), and render() methods.

--- JPong/src/jpong/Game.java
+++ JPong/src/jpong/Game.java
@@ -7,7 +7,7 @@ import jpong.display.Display;
  *
  * @author Ryan Wilson
  */
-public class Game {
+public class Game implements Runnable {
     private boolean isRunning;
     private String windowTitle;
     private int windowWidth;
@@ -23,5 +23,49 @@ public class Game {
         this.windowTitle = windowTitle;
         this.windowWidth = windowWidth;
         this.windowHeight = windowHeight;
+    }
+    
+    
+    private void init(){
+        display = new Display(windowTitle, windowWidth, windowHeight);
+    }
+    
+    private void update(){
+        // stuff to update each tick
+    }
+    
+    private void render(){
+        // stuff to render each tick
+    }
+    
+    @Override 
+    public void run(){
+        init();
+        while(isRunning){
+            update();
+            render();
+        }
+        stop();
+    }
+    
+    public synchronized void start(){
+        if(isRunning){
+            return;
+        }
+        isRunning = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+    
+    public synchronized void stop(){
+        if(!isRunning){
+            return;
+        }
+        isRunning = false;
+        try {
+            thread.join();
+        } catch(InterruptedException ie){
+            ie.printStackTrace();
+        }
     }
 }
