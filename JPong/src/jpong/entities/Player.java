318538e06ae64a0bf51244ea3ee0b569a8aa2458
onlinePB <ryanwilson210400@gmail.com>
2019-03-03 13:00:47 +0000

    Created the Javadoc comments for the Player class Created the Javadoc comments for the public methods of the Player class

--- JPong/src/jpong/entities/Player.java
+++ JPong/src/jpong/entities/Player.java
@@ -10,6 +10,16 @@ import jpong.Game;
 public class Player extends Paddle {
     private boolean arrowControls;
     
+    /**
+     * Constructs the Player object
+     * @param x the x position of the player's paddle
+     * @param y the y position of the player's paddle
+     * @param width the width of the player's paddle in pixels
+     * @param height the height of the player's paddle in pixels
+     * @param colour the colour of the player's paddle
+     * @param game the Game object
+     * @param arrowControls if true, the player uses the left and right arrow keys to move, else they use A and D
+     */
     public Player(int x, int y, int width, int height, Color colour,Game game, boolean arrowControls){
         super(x, y, width, height, colour, game);
         this.arrowControls = arrowControls;
