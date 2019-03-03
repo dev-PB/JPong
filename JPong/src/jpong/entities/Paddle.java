5942c6fe79b5b29834fb175ee15deb55ec761fe6
onlinePB <ryanwilson210400@gmail.com>
2019-03-03 12:56:24 +0000

    Created Javadoc comments for Paddle class Created the Javadoc comments for all of the public methods in the Paddle class

--- JPong/src/jpong/entities/Paddle.java
+++ JPong/src/jpong/entities/Paddle.java
@@ -19,6 +19,15 @@ public class Paddle {
     protected Game game;
     protected final int DEFAULT_X;
     
+    /**
+     * Constructs the Paddle object
+     * @param x the x position of the paddle
+     * @param y the y position of the paddle
+     * @param width the width of the paddle in pixels
+     * @param height the height of the paddle in pixels
+     * @param colour the colour of the paddle
+     * @param game the Game object
+     */
     public Paddle(int x, int y, int width, int height, Color colour, Game game){
         this.x = x;
         this.y = y;
@@ -30,16 +39,29 @@ public class Paddle {
         moveSpeed = 2;
         DEFAULT_X = (game.getWindowWidth() / 2) - (width/2);
     }
+    
+    /**
+     * Ticks the paddle
+     */
     public void update(){
         movePaddle();
         
     }
     
+    /**
+     * Renders the paddle to the screen
+     * @param graphics the Graphics object
+     */
     public void render(Graphics graphics){
         graphics.setColor(colour);
         graphics.fillRect(x,y,width,height);
     }
     
+    /**
+     * Returns true if the paddle has enough points to win
+     * @param scoreToWin the amount of points needed to return true
+     * @return true if points field is greater than or equal to the scoreToWin parameter
+     */
     public boolean checkIfWin(int scoreToWin){
         if (points >= scoreToWin){
             return true;
@@ -48,39 +70,72 @@ public class Paddle {
         }
     }
     
+    /**
+     * Moves the paddle
+     */
     public void movePaddle(){
         
     }
     
+    /**
+     * Gets the colour of the paddle
+     * @return the Color object
+     */
     public Color getColour(){
         return colour;
     }
     
+    /**
+     * Gets the x position of the paddle
+     * @return the x position of the paddle
+     */
     public int getX(){
         return x;
     }
     
+    /**
+     * Gets the y position of the paddle
+     * @return the y position of the paddle
+     */
     public int getY(){
         return y;
     }
     
+    /**
+     * Gets the width of the paddle
+     * @return the width of the paddle in pixels
+     */
     public int getWidth(){
         return width;
     }
     
+    /**
+     * Gets the height of the paddle
+     * @return the height of the paddle in pixels
+     */
     public int getHeight(){
         return height;
     }
     
+    /**
+     * Gets the amount of points the paddle has
+     * @return the amount of points the paddle has
+     */
     public int getPoints(){
         return points;
     }
     
+    /**
+     * Increments the paddle's points by 1
+     */
     public void incrementPoints(){
         points++;
         
     }
     
+    /**
+     * Resets the paddle's x position
+     */
     public void reset(){
         x = DEFAULT_X;
     }
